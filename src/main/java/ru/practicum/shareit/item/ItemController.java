a43fdf41252a1dff9d40461d3ba6db4d0a2dc92a
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-05-25 22:41:45 +0300

    feat: Контроллеры ItemController и UserController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,69 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.model.ItemMapper;
+import ru.practicum.shareit.item.service.ItemService;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<ItemDto> createItem(@Valid @RequestHeader("X-Sharer-User-Id") Long userId,
+                                              @Valid @RequestBody ItemDto itemDto) {
+        Item createdItem = itemService.createItem(userId, ItemMapper.toItem(itemDto));
+        log.info("Received GET request createItem with userId: {}", userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ItemMapper.toItemDto(createdItem));
+    }
+
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<ItemDto> patchItem(@Valid @RequestHeader("X-Sharer-User-Id") Long userId,
+                                             @Valid @PathVariable Long itemId,
+                                             @RequestBody ItemDto itemDto) {
+        Item updatedItem = itemService.patchItem(userId, itemId, ItemMapper.toItem(itemDto));
+        log.info("Received PATCH request patchItem for itemId: {}", itemId);
+        return ResponseEntity.ok(ItemMapper.toItemDto(updatedItem));
+    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemDto> getItemById(@Valid @PathVariable Long itemId) {
+        Item item = itemService.getItemById(itemId);
+        log.info("Received GET request for item with itemId: {}", itemId);
+        return ResponseEntity.ok(ItemMapper.toItemDto(item));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemDto>> getAllItems(@Valid @RequestHeader("X-Sharer-User-Id") Long userId) {
+        List<Item> items = itemService.getAllItems(userId);
+        List<ItemDto> itemDtoList = items.stream()
+                .map(ItemMapper::toItemDto)
+                .collect(Collectors.toList());
+        log.info("Received GET request getAllItems for userId: {}", userId);
+        return ResponseEntity.ok(itemDtoList);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ItemDto>> searchItems(@RequestParam("text") String text) {
+
+        List<Item> items = itemService.searchItems(text);
+        List<ItemDto> itemDtoList = items.stream()
+                .map(ItemMapper::toItemDto)
+                .collect(Collectors.toList());
+        log.info("Received GET request searchItems with text: {}", text);
+        return ResponseEntity.ok(itemDtoList);
+    }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,62 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.model.UserMapper;
+import ru.practicum.shareit.user.service.UserService;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(path = "/users")
+@Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> create(@Valid @RequestBody UserDto userDto) {
+        User user = UserMapper.toUser(userDto);
+        User createdUser = userService.createUser(user);
+        log.info("Получен POST запрос create");
+        return new ResponseEntity<>(UserMapper.toUserDto(createdUser), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserDto> patchUserById(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        User patchedUser = userService.patchUserById(userId, UserMapper.toUser(userDto));
+        log.info("Получен PATCH запрос patchUserById с userId: {}", userId);
+        return new ResponseEntity<>(UserMapper.toUserDto(patchedUser), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@Valid @PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        log.info("Получен GET запрос getUserById с userId: {}", userId);
+        return new ResponseEntity<>(UserMapper.toUserDto(user), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAll() {
+        List<UserDto> userDtoList = userService.getAllUsers().stream()
+                .map(UserMapper::toUserDto)
+                .collect(Collectors.toList());
+        log.info("Получен GET запрос getAll");
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@Valid @PathVariable Long userId) {
+        userService.deleteUser(userId);
+        log.info("Получен DELETE запрос deleteUser с userId: {}", userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
