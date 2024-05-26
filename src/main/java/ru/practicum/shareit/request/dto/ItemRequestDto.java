79bec2ba4a2a6c7bee0434455d4f9a569a8fff3a
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-05-26 12:58:55 +0300

    feat: ItemRequestDto обозначен @Value и @Builder

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
+import lombok.Value;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
+@Value
+@Builder
 public class ItemRequestDto {
     Long id; // Уникальный идентификатор запроса;
     String description; // Текст запроса, содержащий описание требуемой вещи;
