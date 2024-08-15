2f4114b35599e2f2aa3f821377958ad1e862335b
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-08-15 03:48:04 +0300

    ref: formating queries ItemRepository

--- src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
@@ -12,14 +12,21 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    @Query("SELECT i FROM Item i WHERE i.ownerId = :ownerId")
+    @Query("select i from Item i where i.ownerId = :ownerId")
     List<Item> getAllByUserId(@Param("ownerId") Long ownerId);
 
-    @Query("SELECT i FROM Item i WHERE (LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%'))) AND i.available = TRUE")
+    @Query("select i " +
+            "from Item i " +
+            "where (lower(i.name) like lower(concat('%', :text, '%')) " +
+            "or lower(i.description) like lower(concat('%', :text, '%'))) " +
+            "and i.available = true")
     List<Item> search(@Param("text") String text);
 
     boolean existsById(@Param("id") Long id);
 
-    @Query("SELECT i FROM Item i WHERE i.ownerId = :ownerId and i.id = :itemId")
+    @Query("select i " +
+            "from Item i " +
+            "where i.ownerId = :ownerId " +
+            "and i.id = :itemId")
     Optional<Item> findItemByOwnerId(@Param("ownerId") Long ownerId, @Param("itemId") Long itemId);
 }
