0b8f3c4ffc874f1efd23e28e13373ca69e906984
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-08-25 22:53:57 +0300

    ref: Логи для ItemRequestController

--- gateway/src/main/java/ru/practicum/gateway/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/gateway/request/ItemRequestController.java
@@ -6,39 +6,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.gateway.request.dto.CreateItemRequestDto;
 
-
+@Slf4j
 @RestController
 @RequestMapping(path = "/requests")
-@Slf4j
 @RequiredArgsConstructor
 public class ItemRequestController {
 
     private final ItemRequestClient itemRequestClient;
 
     @PostMapping
-    public ResponseEntity<Object> createItemRequest(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                 @RequestBody CreateItemRequestDto creationDto) {
+    public ResponseEntity<Object> createItemRequest(
+            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @RequestBody CreateItemRequestDto creationDto) {
+        log.info("POST Item Request from Gateway with userId: {}", userId);
         return itemRequestClient.createItemRequest(userId, creationDto);
     }
 
     @GetMapping("/{requestId}")
-    public ResponseEntity<Object> getItemRequestById(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                  @PathVariable Long requestId) {
-
+    public ResponseEntity<Object> getItemRequestById(
+            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @PathVariable Long requestId) {
+        log.info("GET Item Request with requestId: {} by userId: {}", requestId, userId);
         return itemRequestClient.getItemRequestById(userId, requestId);
     }
 
     @GetMapping
     public ResponseEntity<Object> getItemRequestByOwner(
-            @RequestHeader("X-Sharer-User-Id") Long userId
-    ) {
+            @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET Item Requests from Gateway by owner userId: {}", userId);
         return itemRequestClient.getItemRequestByOwner(userId);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Object> getAllItemRequests(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                                     @RequestParam int from,
-                                                     @RequestParam int size) {
+    public ResponseEntity<Object> getAllItemRequests(
+            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @RequestParam int from,
+            @RequestParam int size) {
+        log.info("GET all Item Requests from Gateway by userId: {} from: {} size: {}", userId, from, size);
         return itemRequestClient.getAllItemRequests(userId, from, size);
     }
 }
