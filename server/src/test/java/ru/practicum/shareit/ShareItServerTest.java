40f8e1e61ac5efeb3d5c0ddd8513ec55cfcd71fa
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-08-26 01:58:15 +0300

    fix: fix Poms for GitHub

--- server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -2,9 +2,30 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.practicum.shareit.booking.mapper.BookingMapper;
+import ru.practicum.shareit.item.comment.mapper.CommentMapper;
+import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.user.mapper.UserMapper;
 
 @SpringBootTest
 public class ShareItServerTest {
+
+    @MockBean
+    BookingMapper bookingMapper;
+
+    @MockBean
+    ItemMapper itemMapper;
+
+    @MockBean
+    CommentMapper commentMapper;
+
+    @MockBean
+    ItemRequestMapper itemRequestMapper;
+
+    @MockBean
+    UserMapper userMapper;
 
     @Test
     void loadContext() {
